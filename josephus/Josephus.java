c9c2ea4d4940e0658ccae2dfb7027983ca48336c
Angeliz <471330418@qq.com>
2017-09-21 21:49:26 +0800

    add

--- josephus/Josephus.java
+++ josephus/Josephus.java
@@ -1,7 +1,8 @@
 package josephus;
-
+import josephus.SinglyList;
 public class Josephus {
 	public Josephus(int number,int start,int distance){
+//		SinglyList<String> list=new SinglyList<String>(number);
 		
 	}
 	public static void main(String[] args) {
--- josephus/SinglyList.java
+++ josephus/SinglyList.java
@@ -1,6 +1,6 @@
 package josephus;
 
-public class SinglyList<String>{
+public class SinglyList<String> extends Object{
 	public String data;							 //数据域
 	public SinglyList<String> next;				 //地址域
 	public SinglyList(String data,SinglyList<String> next){  //构造结点
@@ -25,4 +25,62 @@ public class SinglyList<String>{
 		return this.head.next==null;
 	}
 	//存储
+	public String get(int i){					  //返回第i个元素，若i越界，则返回null
+		SinglyList<String> p=this.head.next;	  
+		for(int j=0;p!=null&&j<i;j++){			  //遍历单链表，返回第i个元素
+			p=p.next;
+		}
+		return (i>=0 && p!=null)?p.data:null;
+	}
+	public void set(int i,String x){
+		
+	}
+//	public int size(){}
+	
+//	public String toString(){
+//		String str=this.getClass().getName()+"(";		//返回类名
+//		for (SinglyList<String> p=this.head.next; p!=null; p=p.next){
+//			str+=p.data.toString();
+//			if(p.next!=null){
+//				str+=",";
+//			}
+//			return str+")";
+//		}
+//	}
+	
+	//插入
+	public SinglyList<String> insert(int i,String x){
+		if(x==null){
+			throw new NullPointerException("x==null");      //抛出空对象异常
+		}
+		SinglyList<String> front=this.head;
+		for(int j=0;front.next!=null&&j<i;j++){
+			front=front.next;
+		}
+		front.next=new SinglyList<String>(x,front.next);
+		return front.next;
+	}
+	public SinglyList<String> insert(String x){
+		return insert(Integer.MAX_VALUE,x);
+	}
+	
+	//删除
+	public String remove(int i){							//删除第i个元素，若i越界，则返回null
+		SinglyList<String> front=this.head;
+		for(int j=0;front.next!=null&&j<i;j++){
+			front=front.next;
+		}
+		if(i>=0&&front.next!=null){
+			String old=front.next.data;
+			front.next=front.next.next;
+			return old;
+		}
+		return null;
+	}
+	
+	public void clear(){
+		this.head.next=null;
+	}
+	
+	//顺序查找和基于查找算法的操作
 }
