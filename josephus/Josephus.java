86dce458b248388745eff2350f51d09ccfdad2b9
Angeliz <471330418@qq.com>
2017-09-23 14:49:34 +0800

    改好了bug

--- josephus/Josephus.java
+++ josephus/Josephus.java
@@ -4,20 +4,24 @@ public class Josephus {
 	int number;
 	int start;
 	int distance;
-	public Josephus(int number,int start,int distance){
+	public Josephus(int number,int start,int distance){					//有参构造函数
 		this.number=number;
 		this.start=start;
 		this.distance=distance;
 	}
-	public void result(){
-		SinglyList<String> list=new SinglyList<String>();             //实例化空的单链表
-		for(int i=0;i<number;i++){
+	public void result(){												//用来计算最终结果的方法
+		SinglyList<String> list=new SinglyList<String>();               //实例化空的单链表
+		for(int i=0;i<number;i++){										//指定犯人个数并插入单链表
 			list.insert(i,(char)('A'+i)+"");
 		}
-		System.out.println(list.toString());
-		int i=start;
-		while(list.size()>1){
+		System.out.println(list.toString());							//输出插入完成后的单链表
+		int i=start;													//计数起始位置
+		while(list.size()>1){											//多于一个元素时，执行此循环
 			i=(i+distance-1)%list.size();
+//			i=i+distance-1;
+//			if(i>list.size()-1){
+//				i=i-list.size();
+//			}
 			System.out.print("删除"+list.remove(i).toString()+", ");
 			System.out.println(list.toString());
 		}
--- josephus/SinglyList.java
+++ josephus/SinglyList.java
@@ -1,59 +1,47 @@
 package josephus;
 
 public class SinglyList<String> extends Object{
-	public String data;							 //数据域
-	public SinglyList<String> next;				 //地址域
-	public SinglyList<String> head;               //头指针
+	public String data;													    //数据域
+	public SinglyList<String> next;				 							//地址域
+	public SinglyList<String> head;               							//头指针
 	
-	public SinglyList(String data,SinglyList<String> next){  //构造结点
+	public SinglyList(String data,SinglyList<String> next){  				//构造结点
 		this.data=data;
 		this.next=next;
 	}
 	
 	//构造函数
-	public SinglyList(){                          //构造空单链表
-		this.head=new SinglyList<String> (null,null);		      //创造头结点，data和next的值均为null	
+	public SinglyList(){                          							//构造空单链表
+		this.head=new SinglyList<String> (null,null);		      			//创造头结点，data和next的值均为null	
 	}
 	
-	public SinglyList(String[] values){           //构造单链表，values数组提供元素
-		this();                                   //创建空单链表，只有头结点(调用无参构造方法)
-		SinglyList<String> rear=this.head;		  //rear指向单链表最后一个结点
-		for(int i=0;i<values.length;i++){
-			rear.next=new SinglyList<String>(values[i],null); //尾插入
-			rear=rear.next;						  //指向新的链尾结点
-		}
-	}
-	public boolean isEmpty(){
-		return this.head.next==null;
-	}
-	//存取
-	public String get(int i){					  //返回第i个元素，若i越界，则返回null
+	//取出元素
+	public String get(int i){					  							//返回第i个元素，若i越界，则返回null
 		SinglyList<String> p=this.head.next;	  
-		for(int j=0;p!=null&&j<i;j++){			  //遍历单链表，返回第i个元素
+		for(int j=0;p!=null&&j<i;j++){			  							//遍历单链表，返回第i个元素
 			p=p.next;
 		}
 		return (i>=0 && p!=null)?p.data:null;
 	}
-	public void set(int i,String x){
-		
-	}
+	
+	//计算size
 	public int size(){
-		SinglyList<String> p=head;
-		int n=0;
-		while(p!=null){
+		SinglyList<String> p=head;											//p指向头结点
+		int n=0;								
+		while(p!=null){														//通过遍历计算
 			n++;
 			p=p.next;
 		}
-		return n;
+		return n-1;															//将最后一个null也算进去了所以减一
 	}
 	
 	//覆盖Object类的toString方法
 	public java.lang.String toString(){
 		java.lang.String str="(";
-		for(SinglyList<String> p=this.head.next; p!=null; p=p.next){
+		for(SinglyList<String> p=this.head.next; p!=null; p=p.next){		//遍历单链表
 			str=str+p.data.toString();
 			if(p.next!=null){
-				str+=",";
+				str+=",";													//不是最后一个节点时加分隔符
 			}
 		}
 		return str+")";
@@ -63,9 +51,9 @@ public class SinglyList<String> extends Object{
 	//插入
 	public SinglyList<String> insert(int i,String x){
 		if(x==null){
-			throw new NullPointerException("x==null");      //抛出空对象异常
+			throw new NullPointerException("x==null");      				//抛出空对象异常
 		}
-		SinglyList<String> front=this.head;					//指向头结点
+		SinglyList<String> front=this.head;									//指向头结点
 		for(int j=0;(front.next!=null)&&j<i;j++){
 			front=front.next;
 		}
@@ -77,12 +65,12 @@ public class SinglyList<String> extends Object{
 	}
 	
 	//删除
-	public String remove(int i){							//删除第i个元素，若i越界，则返回null
-		SinglyList<String> front=this.head;
-		for(int j=0;front.next!=null&&j<i;j++){
+	public String remove(int i){											//删除第i个元素，若i越界，则返回null
+		SinglyList<String> front=this.head;									//指向头结点
+		for(int j=0;front.next!=null&&j<i;j++){								//遍历寻找i-1个结点
 			front=front.next;
 		}
-		if(i>=0&&front.next!=null){
+		if(i>=0&&front.next!=null){											//若front的后继节点存在，就删除
 			String old=front.next.data;
 			front.next=front.next.next;
 			return old;
